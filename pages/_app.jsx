5db9ddeb6ba83f6730a3224f03bd531fdeb78373
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-04-28 20:45:05 +0700

    Disable no props spreading linting error in App

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import 'normalize.css';
 import 'styles/global.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
