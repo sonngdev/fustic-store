ca95a0f00fc00dc7450bf2ce9f8aa85efaa70bf8
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-04-30 18:42:57 +0700

    Create Button component

---
+++ components/button.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+export default function Button(props) {
+  return (
+    <>
+      <button {...props} />
+
+      <style jsx>
+        {`
+          button {
+            padding: 10px 15px;
+            color: var(--color-background);
+            background-color: var(--color-text);
+            border-radius: 3px;
+          }
+        `}
+      </style>
+    </>
+  );
+}
