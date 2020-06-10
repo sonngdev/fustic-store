c709eb3109c4c523741539cb4d3c8c56b0b516c4
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-06-10 10:33:53 +0700

    Make transaction work in sandbox app

--- pages/paypal.jsx
+++ pages/paypal.jsx
@@ -3,13 +3,30 @@ import Head from 'next/head';
 
 export default function Paypal() {
   useEffect(() => {
-    window.paypal.Buttons().render('#paypal-button');
+    const createOrder = (data, actions) => {
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '8'
+          }
+        }]
+      });
+    };
+
+    const onApprove = (data, actions) => {
+      return actions.order.capture().then(function(details) {
+        // This function shows a transaction success message to your buyer.
+        alert('Transaction completed by ' + details.payer.name.given_name);
+      });
+    }
+
+    window.paypal.Buttons({ createOrder, onApprove }).render('#paypal-button');
   });
 
   return (
     <>
       <Head>
-        <script src="https://www.paypal.com/sdk/js?client-id=sb"></script>
+        <script src="https://www.paypal.com/sdk/js?client-id=AVhHplhUoO7AJdreVmzdYVx_gxNs_TdLZtf81Zuv7AnX8WvEsErqDTOjdBG2s5IlgvOlvNwjwzbeKM6a"></script>
       </Head>
 
       <h1>Hello Paypal</h1>
