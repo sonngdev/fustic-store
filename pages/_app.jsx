960b7f1e1a3349a06b5f1265c85a04c728e68710
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-06-26 22:08:48 +0700

    Move head tags into App

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,13 +2,24 @@
 /* eslint-disable react/forbid-prop-types */
 
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 
 import 'normalize.css';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import 'styles/global.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Fustic Store</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 App.propTypes = {
