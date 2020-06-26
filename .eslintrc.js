96ec05f0518dea660e18658f5126e3dc6121ca7d
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-06-26 23:12:47 +0700

    Turn off eslint `import/prefer-default-export`

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     'react',
   ],
   rules: {
+    'import/prefer-default-export': 'off',
     'react/jsx-one-expression-per-line': 'off',
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
