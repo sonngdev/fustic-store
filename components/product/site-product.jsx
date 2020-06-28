eefe1f015f1cf3f7fdf5832538822ff23e06742c
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-06-28 22:20:39 +0700

    Fix font-size & line-height

--- components/product/site-product.jsx
+++ components/product/site-product.jsx
@@ -64,7 +64,7 @@ export default function SiteProduct({ product }) {
             .name-category {
               font-size: var(--fontsize-sm);
               font-weight: var(--fontweight-bold);
-              line-height: 0.75rem;
+              line-height: var(--lineheight-sm);
             }
 
             .price {
