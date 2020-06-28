3b851c292f3a35f4b0d7461d8c8d652528655cf7
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-06-28 17:31:28 +0700

    Make background video truly fullscreen (when iPhone X screen rotates)

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,11 +34,15 @@ export default function Home({ products }) {
           background-size: cover;
           background-position: center;
 
+          /**
+           * Fullscreen video background
+           * https://codepen.io/abennington/pen/ZONqqv?editors=0100
+           */
           :global(.background-video iframe) {
-            width: 100%;
-            height: 100%;
-            min-height: 100vh;
+            width: 100vw;
+            height: 56.25vw; /* Given a 16:9 aspect ratio, 9/16*100 = 56.25 */
             min-width: 177.77vh; /* Given a 16:9 aspect ratio, 16/9*100 = 177.77 */
+            min-height: 100vh;
             position: absolute;
             top: 50%;
             left: 50%;
