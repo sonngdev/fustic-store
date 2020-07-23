d02d83b96e7386037405dcbcc41334fef3ba5b5d
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-07-23 11:45:01 +0700

    f d28db44

--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,7 @@
 import { ensureCamel } from 'utils/object';
 
 export async function get(url) {
-  const req = await fetch(url);
+  const req = await fetch(url, { headers: { Accept: 'application/json' } });
   const res = await req.json();
   return ensureCamel(res);
 }
