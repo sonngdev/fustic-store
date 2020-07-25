f816ee33d3e36ddc901ceb0de8676c581dcfc81c
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-07-25 12:28:01 +0700

    feat: add to cart action

--- store/reducer.js
+++ store/reducer.js
@@ -1,10 +1,16 @@
+import { ADD_TO_CART } from './actions';
+
 const defaultState = {
-  categories: [],
   cart: [],
 };
 
 export default function reducer(state = defaultState, action) {
   switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cart: state.cart.concat(action.payload),
+      };
     default:
       return state;
   }
