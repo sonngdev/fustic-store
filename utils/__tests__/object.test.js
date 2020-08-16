13a0d5e60f671ff52a25862a2e8b045e7a96fabf
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-08-16 20:06:01 +0700

    feat: ensure snake case object util

--- utils/__tests__/object.test.js
+++ utils/__tests__/object.test.js
@@ -1,4 +1,4 @@
-import { ensureCamel } from '../object';
+import { ensureCamel, ensureSnake } from '../object';
 
 describe('ensureCamel', () => {
   it('ignores non-object arguments', () => {
@@ -20,5 +20,28 @@ describe('ensureCamel', () => {
     expect(ensureCamel([{ foo_bar: 1 }])).toEqual([{ fooBar: 1 }]);
     expect(ensureCamel([{ foo_bar: 1 }, { baz_qux: 2 }])).toEqual([{ fooBar: 1 }, { bazQux: 2 }]);
     expect(ensureCamel([{ foo_bar: { baz_qux: 2 } }])).toEqual([{ fooBar: { bazQux: 2 } }]);
+  });
+});
+
+describe('ensureSnake', () => {
+  it('ignores non-object arguments', () => {
+    expect(ensureSnake('string')).toBe('string');
+    expect(ensureSnake(42)).toBe(42);
+  });
+
+  it('ingores null values', () => {
+    expect(ensureSnake(null)).toBeNull();
+  });
+
+  it('ensures all object keys are in snake_case', () => {
+    expect(ensureSnake({ fooBar: 1 })).toEqual({ foo_bar: 1 });
+    expect(ensureSnake({ fooBar: { bazQux: 2 } })).toEqual({ foo_bar: { baz_qux: 2 } });
+  });
+
+  it('works on arrays', () => {
+    expect(ensureSnake([1, 2, 3])).toEqual([1, 2, 3]);
+    expect(ensureSnake([{ fooBar: 1 }])).toEqual([{ foo_bar: 1 }]);
+    expect(ensureSnake([{ fooBar: 1 }, { bazQux: 2 }])).toEqual([{ foo_bar: 1 }, { baz_qux: 2 }]);
+    expect(ensureSnake([{ fooBar: { bazQux: 2 } }])).toEqual([{ foo_bar: { baz_qux: 2 } }]);
   });
 });
