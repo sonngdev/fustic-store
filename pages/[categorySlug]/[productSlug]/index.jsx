aafede7f930e6c3cacc15dbcb0be1ae36b18a3a6
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-08-22 18:06:24 +0700

    feat: incremental SSG for pages

--- pages/[categorySlug]/[productSlug]/index.jsx
+++ pages/[categorySlug]/[productSlug]/index.jsx
@@ -76,5 +76,6 @@ export async function getStaticProps({ params }) {
     props: {
       product,
     },
+    revalidate: true,
   };
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -85,5 +85,6 @@ export async function getStaticProps() {
       products,
       vimeoThumbnailUrl,
     },
+    revalidate: true,
   };
 }
