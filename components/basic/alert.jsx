357b6cb08e95615b4751febecab31bb1febc0b52
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-09-20 17:50:16 +0700

    feat: add Alert component

---
+++ components/basic/alert.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/no-danger */
+
+import PropTypes from 'prop-types';
+
+function Alert({ message }) {
+  return (
+    <div className="alert">
+      {message}
+
+      <style jsx>
+        {`
+        .alert {
+          background-color: #F1B5A9;
+          color: #721c24;
+          padding: 1.2em 2em;
+
+          @media screen and (min-width: 1200px) {
+            font-size: var(--fontsize-md);
+            font-weight: var(--fontweight-bold);
+            padding: 2em;
+          }
+        }
+        `}
+      </style>
+    </div>
+  );
+}
+
+Alert.propTypes = {
+  message: PropTypes.node.isRequired,
+};
+
+export default Alert;
