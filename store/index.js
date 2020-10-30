3ffcccb41a04ba7ac81f170cbd09d049d7418f7b
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-10-31 00:32:47 +0700

    fix: HUGE!!! null initial state
    Context:
    - We want to pre-render some head tags so that Facebook bot and the
    likes can make use of og tags.
    - In order to do that, we must get rid of PersistGate, else
    everything will be bootstrapped by JavaScript on the client later.
    In other words, links will not have previews.
    - The removal of PersistGate results in a strange error. Initial
    state doesn't have the shape defined in reducers, only the _persist
    key.
    
    Finally I found the cause, and very accidentally. When I was trying
    to use redux-logger and delete this line, everything works fine.
    For some reasons, browser's redux devtools extension caused the above
    behavior.

--- store/index.js
+++ store/index.js
@@ -17,7 +17,6 @@ const makeStore = ({ isServer }) => {
 
   const store = createStore(
     persistedReducer,
-    typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   );
   store.__persistor = persistStore(store);
 
