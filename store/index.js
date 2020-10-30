44c7dd196f26e68de3ba5c9e115896e678d20782
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-10-31 01:16:25 +0700

    feat: use redux-logger for development env

--- store/index.js
+++ store/index.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-underscore-dangle */
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { createWrapper } from 'next-redux-wrapper';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
 import reducer from './reducer';
 
 // https://github.com/fazlulkarimweb/with-next-redux-wrapper-redux-persist
@@ -15,11 +16,15 @@ const makeStore = ({ isServer }) => {
     storage,
   }, reducer);
 
-  const store = createStore(
-    persistedReducer,
-  );
+  const middlewares = [];
+  if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+  }
 
-  return store;
+  return createStore(
+    persistedReducer,
+    applyMiddleware(...middlewares),
+  );
 };
 
 export const wrapper = createWrapper(makeStore);
